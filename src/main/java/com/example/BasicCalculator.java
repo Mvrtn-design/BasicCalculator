d8a8b7a79d02729680c7cad6a66719790c153975
Mvrtn-design <martinmochi56@gmail.com>
2024-09-17 17:01:21 +0200

    Initial commit with basic calculator funtionality

---
+++ src/main/java/com/example/BasicCalculator.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class BasicCalculator {
+    private static int add(int a, int b) {
+        return (a + b);
+    }
+
+    private static double substract(int a, int b) {
+        return (a - b);
+    }
+
+    private static long multiply(int a, int b) {
+        return (a * b);
+    }
+
+    private static double divide(int a, int b) {
+        return (a / b);
+    }
+
+    public static void basicArithmeticOperations(int a, int b) {
+        // ADD
+        add(a, b);
+
+        //SUBSTRACT
+        substract(a, b);
+        
+        multiply(a, b);
+        divide(a, b);
+
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 34;
+        basicArithmeticOperations(a, b);
+    }
+}
